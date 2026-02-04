b3e80b38815c0f911c812ef2b886b2125493b27b
GCaggianese <german.caggianese@pm.me>
2026-02-04 02:24:25 -0300

    new posts + updated copyright year

--- website/_layouts/replace_home.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env sh
-
-find . -type f -name "*.html" -exec sed -i.bak '
-/<a class="btn zoombtn" href="{{site\.url}}">/{
-N
-N
-s|<a class="btn zoombtn" href="{{site\.url}}">\n *<i class="fa fa-home"><\/i>\n *<\/a>|{% if site.active_lang == "en" %}\n\n                    <a class="btn zoombtn" href="{{site.url}}">\n                        <i class="fa fa-home"><\/i>\n                    <\/a>\n\n                    {% else %}\n\n                    <a class="btn zoombtn" href="{{site.url}}/{{active_lang}}">\n                        <i class="fa fa-home"><\/i>\n                    <\/a>\n\n                    {% endif %}|
-}' {} \;
